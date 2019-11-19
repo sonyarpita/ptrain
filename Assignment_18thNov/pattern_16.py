94a631c11a5ec4a75f24d82b61527e3cfd6f7e85
Sony Arpita Das <sonyarpita@gmail.com>
2019-11-19 04:08:19 -0800

    Rest of the scripts

---
+++ Assignment_18thNov/pattern_16.py
@@ -0,0 +1,11 @@
+'''
+1
+12
+123
+1234
+12345
+'''
+for i in range(0,7):
+    for j in range(1,i):
+        print(j,end="")
+    print("\n")
---
+++ Assignment_18thNov/pattern_17b.py
@@ -0,0 +1,11 @@
+'''
+12345
+1234
+123
+12
+1
+'''
+for i in range(6,0,-1):
+    for j in range(1,i):
+        print(j,"",end="")
+    print("\n")
