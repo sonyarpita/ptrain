5646147122fb033d676dc48380d1ad4e1007bbf8
Sony <sonyarpita@gmail.com>
2019-11-23 14:43:05 +0530

    Updaed assignment file

--- Assignment_22ndNov/first_prime_numbers_among_1000_numbers.py
+++ Assignment_22ndNov/first_prime_numbers_among_1000_numbers.py
@@ -1,15 +1,18 @@
-start=int(input("Enter start of the range of 1000 numbers"))
-stop=int(input("Enter end of the range of 1000 numbers"))
+start=int(input("Enter start of the range of 1000 numbers: "))
+stop=int(input("Enter end of the range of 1000 numbers: "))
 diff=stop-start
-counter=0
-if diff==1000:
-    for num in range(start,stop):
-        for j in (1,num+1):
-            modulo=num%j
-            if modulo==0:
-                counter=counter+1
-        if counter==2:
-            print("Even numbers in the range->",start,"-",stop,"=",num)
+#counter=0
+if diff==100:
+	for num in range(start,stop):
+		#print("Value of Number = ",num)
+		counter=0
+		for j in range(1,num+1):
+		#print("Value of J = ",j)
+			modulo=num%j
+			if modulo==0:
+				counter=counter+1
+		print("Number of divisors of",num,"=",counter)
+		if counter==2:
+			print("Prime numbers in the range->",start,"-",stop,"=",num)
 else:
     print("Please enter a valid range")
-
