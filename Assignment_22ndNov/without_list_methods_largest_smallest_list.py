fb7b26747873ea3dd494501f75331d0e77bc7512
Sony <sonyarpita@gmail.com>
2019-11-25 23:09:50 +0530

    without list methods largest smallest

--- Assignment_22ndNov/without_list_methods_largest_smallest_list.py
+++ Assignment_22ndNov/without_list_methods_largest_smallest_list.py
@@ -1,60 +1,44 @@
 num_of_nums=int(input("Enter number of numbers to be sorted: "))
 user_input_num_list=[]
-
+simi=[]
 def enter_list_elements(x):
-    for i in range(x):
-        i=int(input("Enter elements: "))
-        user_input_num_list.append(i)
-        return user_input_num_list
+	for i in range(x):
+		i=int(input("Enter elements: "))
+		user_input_num_list.append(i)
+	return user_input_num_list
 
-enter_list_elements(num_of_nums)
-print("Largest Number=",max(user_input_num_list))
-print("Smallest Number=",min(user_input_num_list))
-print(user_input_num_list.index(2,2)) 
+print("List=",enter_list_elements(num_of_nums))
 
-'''def equal_elements(user_input_num_list):
-    length_of=len(user_input_num_list)
-    print("Number of elements=",length_of)
-    for i in range(len(user_input_num_list)):
-        #length_of=0
-        for j in range(1,len(user_input_num_list)+1):
-           if user_input_num_list[i]==user_input_num_list[-j]:
-                print("Similar elements=",user_input_num_list[i])
-                length_of=length_of-1
-                #print("Similar element=",user_input_num_list[i])
-    return length_of
-'''
-'''def largest(user_input_num_list):
-    length_of=equal_elements(user_input_num_list)
-    print("Number of unequal elements=",length_of)
-    #for i in range(len(user_input_num_list)):
-    for i in range(length_of):
-        counter=0
-        for j in range(1,length_of+1):
-            #print(user_input_num_list[i],user_input_num_list[-j])
-            if user_input_num_list[i] > user_input_num_list[-j]:
-                counter=counter+1
-                #print("For",user_input_num_list[i],"Counter=",counter)
-        if counter==length_of-1:
-            #print("Inside Loop",user_input_num_list[i])
-            return user_input_num_list[i]
+
+def similar(user_input_num_list):
+	for i in range(len(user_input_num_list)):
+		for j in range(1,len(user_input_num_list)+1):
+			if i!=len(user_input_num_list)-j and user_input_num_list[i]==user_input_num_list[-j]:
+				simi.append(user_input_num_list[i])
+	return simi
+
+
+def largest(user_input_num_list):
+	for i in range(len(user_input_num_list)):
+		counter=0
+		for j in range(1,len(user_input_num_list)+1):
+			if user_input_num_list[-j]<=user_input_num_list[i] and i!=len(user_input_num_list)-j:
+				counter=counter+1
+			if counter==len(user_input_num_list)-1:
+				return user_input_num_list[i]
+
 
 def smallest(user_input_num_list):
-    length_of=equal_elements(user_input_num_list)
-    #for i in range(len(user_input_num_list)):
-    for i in range(length_of):
-        counter=0
-        #for j in range(1,len(user_input_num_list)+1):
-        for j in range(1,length_of+1):
-            #print(user_input_num_list[i],user_input_num_list[-j])
-            if user_input_num_list[i] < user_input_num_list[-j]:
-                counter=counter+1
-        #       if counter==len(user_input_num_list)-1:
-        if counter==length_of-1:
-            return user_input_num_list[i]
-#equal_elements(user_input_num_list)
-print("Largest Number=",largest(user_input_num_list))
-print("Smallest Number=",smallest(user_input_num_list))'''
-#print("Equal Elements=",equal_elements(user_input_num_list))
+	for i in range(len(user_input_num_list)):
+		counter=0
+		for j in range(1,len(user_input_num_list)+1):
+			if user_input_num_list[-j]>=user_input_num_list[i] and i!=len(user_input_num_list)-j:
+				counter=counter+1
+			if counter==len(user_input_num_list)-1:
+				return user_input_num_list[i]
+
+print("Similar elements=",similar(user_input_num_list))
+print("Largest element=",largest(user_input_num_list))
+print("Smallest element=",smallest(user_input_num_list))
 
 
