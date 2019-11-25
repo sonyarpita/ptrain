f8f7bb0ea210dc5331b7705008de924956463db9
Sony Arpita Das <sonyarpita@gmail.com>
2019-11-24 21:00:30 -0800

    user modules 2 and 3 Part 2

---
+++ Modules/user_module2.py
@@ -0,0 +1,7 @@
+from calculator import sume,subs,mult,divi
+x=int(input("Enter value for x: "))
+y=int(input("Enter value for y: "))
+print(sume(x,y))
+print(subs(x,y))
+print(mult(x,y))
+print(divi(x,y))
---
+++ Modules/user_module3.py
@@ -0,0 +1,7 @@
+from calculator import *
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+print(sume(x,y))
+print(subs(x,y))
+print(mult(x,y))
+print(divi(x,y))
