cf87e7af790d19afcdc027b230e70f65b3c9283c
Sony <sonyarpita@gmail.com>
2019-12-11 21:40:57 +0530

    Completed assignment 11th Dec

--- Assignment_11thDec/consonants_vowels_count.py
+++ Assignment_11thDec/consonants_vowels_count.py
@@ -1,7 +1,5 @@
 user_word=input("Enter word: ")
 vowel_list=['a','e','i','o','u']
-counter=0
-list_word=[]
 list_word=list(user_word)
 for vowel in range(len(vowel_list)):
     counter=0
@@ -10,14 +8,3 @@ for vowel in range(len(vowel_list)):
             counter+=1
     print(vowel_list[vowel],"occurs",counter)
 
-
-'''   if vowel_list[vowel] in user_input :
-        counter+=1
-        if counter==1:
-            print(vowel_list[vowel],"occurs",counter,"time")
-        else:
-            print(vowel_list[vowel],"occurs",counter,"times")
-list_word=[]
-list_word=list(user_input)
-for i in range(len(list_word)):
-    print(list_word[i])'''
--- Assignment_11thDec/convert_first_letter_in_a_sentence_to_capital_letter.py
+++ Assignment_11thDec/convert_first_letter_in_a_sentence_to_capital_letter.py
@@ -1,1 +1,7 @@
-i
+user_list=list(input("Enter word/sentence: "))
+for i in range(len(user_list)):
+	if ord(user_list[i])==32 and ord(user_list[i+1])>96:
+		asci=ord(user_list[i+1])-32
+		user_list[i+1]=chr(asci)
+str=''.join(user_list)
+print("Evaluated Sentence: ",str)
